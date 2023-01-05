c2c750a0011eb9cb22f7ea6d2719c98e9b7e673f
daurora-net <daurora.net@gmail.com>
2023-01-06 02:55:29 +0900

    reserveページ修正

--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -3,8 +3,21 @@
 @section('content')
 <div class="container">
     <div class="main reserve">
-        <p>{{$reservation->user->name}}様ご予約</p>
-        <p>{{$reservation->shop->name}}</p>
+        <b>{{$reservation->user->name}}様ご予約</b>
+        <table class="reserve_tb" border="1">
+            <tr>
+                <th>店舗</th>
+                <td>{{ $reservation->shop->name }}</td>
+            </tr>
+            <tr>
+                <th>予約日</th>
+                <td>{{ $reservation->started_at->format('Y年m月d日H:i')}}</td>
+            </tr>
+            <tr>
+                <th>Number</th>
+                <td>{{ $reservation->num_of_users }}人</td>
+            </tr>
+        </table>
     </div>
 </div>
 @endsection
