a2f126cd0f85b42a6d4a6ab32d69d7b4493a208e
daurora-net <daurora.net@gmail.com>
2023-01-15 06:00:33 +0900

    タイトルタグ修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Rese</title>
+  <title>@yield('title') | Rese</title>
   <link rel="stylesheet" href="/css/style.css">
   <link rel="stylesheet" href="/css/auth.css">
   <link rel="stylesheet" href="/css/modal.css">
