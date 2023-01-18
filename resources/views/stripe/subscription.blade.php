6aa085a1b23b99e34e8d15451697afd824da369a
daurora-net <daurora.net@gmail.com>
2023-01-18 20:57:58 +0900

    決済フォーム、ボタン修正

--- resources/views/stripe/subscription.blade.php
+++ resources/views/stripe/subscription.blade.php
@@ -64,16 +64,16 @@
       }
     </script>
 
-    <div class="container py-3">
-      <h3 class="mb-3">ご登録フォーム</h3>
+    <div class="container">
+      <h3 class="stripe_ttl">有料会員登録フォーム</h3>
       <form action="{{route('stripe.afterpay')}}" method="post" id="payment-form">
         @csrf
         <label for="exampleInputEmail1">お名前</label>
-        <input type="test" class="form-control col-sm-5" id="card-holder-name" required>
+        <input type="test" class="form-control MyNameElement" id="card-holder-name" required>
         <label for="exampleInputPassword1">カード番号</label>
-        <div class="form-group MyCardElement col-sm-5" id="card-element"></div>
+        <div class="form-group MyCardElement" id="card-element"></div>
         <div id="card-errors" role="alert" style='color:red'></div>
-        <button class="btn btn-primary" id="card-button" data-secret="{{ $intent->client_secret }}">送信する</button>
+        <button class="btn_stripe" id="card-button" data-secret="{{ $intent->client_secret }}">送信する</button>
       </form>
     </div>
 
